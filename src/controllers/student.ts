1d88a280e9b6c8d35bf0c6ff6872a049ccc82a17
cristian-rla <Cristianrluquea@hotmail.com>
2025-04-03 14:19:30 -0600

    added controller logic

--- src/controllers/student.ts
+++ src/controllers/student.ts
@@ -6,7 +6,25 @@ class StudentController{
         this.studentService = studentService;
     }
     async getStudentStatus(){
-        this.studentService.getStudentData();
+        const processed = this.studentService.getStudentData().forEach(({ mat, grade, debt}) => {
+        let message;
+            if(grade > 90 && !debt){
+                message = "Te graduaste con honores";
+// l mensaje es "Te graduaste con honores". Si tiene >90 pero adeudo == true "Tenías honores pero tienes un adeudo". 90> >70 independiente del adeudo "Pasaste muy apenas". <70 sin adeudo "Como no tienes adeudo, tienes derecho a un examen de recuperacion". <70  && adeudo "expulsado"
+            } else if(grade > 90 && debt){
+                message = "Tenías honores pero tienes un adeudo";
+            } else if(grade >= 70){
+                message = "Pasaste muy apenas";
+
+            } else if(grade < 70 && !debt){
+                message = "Como no tienes adeudo, tienes derecho a un examen de recuperacion";
+            } else {
+                message = "expulsado";
+
+            }
+            return {mat, message};
+        });        
+        return processed;
 
     }
 
